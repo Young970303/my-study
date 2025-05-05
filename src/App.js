920e6db7a8634bc3df93aaef051a3a47de0c7321
Young970303 <skwndbth88@naver.com>
2025-05-05 15:31:18 +0900

    jsx문법연산

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import './App.css';
+
 function App() {
+  const name = undefined;
   return (
-    <h1><리액트 안녕!/h1>>
+    <>
+      <h1>리액트 안녕!</h1>
       <h2>잘 작동하니?</h2>
-       
-  )
+      <div>{name || '리액트'}</div>
+    </>
+  );
 }
 
 export default App;
