5f57d8a4fe2305759ecd15c90b1e115d084d108c
Young970303 <skwndbth88@naver.com>
2025-05-06 15:06:56 +0900

    리액트명암

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
+<<<<<<< HEAD
 import './App.css';
 
+=======
+<<<<<<< HEAD
+import { Fragment } from "react";
+>>>>>>> 0408a0e (jsx문법 연산자)
 function App() {
   const name = undefined;
   return (
+<<<<<<< HEAD
     <>
       <h1>리액트 안녕!</h1>
       <h2>잘 작동하니?</h2>
       <div>{name || '리액트'}</div>
     </>
   );
+=======
+    <Fragment>
+<h1>리액트 안녕!</h1>
+<h2>잘 작동하니?</h2>
+</Fragment>
+=======
+function App() {
+  return (
+    <h1><리액트 안녕!/h1>>
+      <h2>잘 작동하니?</h2>
+       
+>>>>>>> 184dda7 (jsx 문법)
+  )
+>>>>>>> 0408a0e (jsx문법 연산자)
 }
 
 export default App;
