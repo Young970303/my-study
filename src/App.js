9d24db1e0b7697faa9bdb3c501e15bb8b1a707fc
Young970303 <skwndbth88@naver.com>
2025-05-14 16:13:32 +0900

    컴포넌트만들기

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
-import './App.css';
+import MyComponent from "./MyComponent";
 
-
-function App() {
-  const name = '리액트';
- return ( 
-  <>
-  
-<div  className="react">{name}</div>
-
-{/* 하지만 이런 주석이나 */}
-{/* 이런 주석은 페이지에 그대로 나타나게 됩니다. */}
-<input />
-</>
- );
-
-}
-
+const App = () => {
+  return <MyComponent />;
+};
 
 export default App;
