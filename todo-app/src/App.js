7b8b2520135289e899db55a1e18defd5abae3655
Young970303 <skwndbth88@naver.com>
2025-07-27 15:40:28 +0900

    TodoInsert

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,7 +1,14 @@
 import TodoTemplate from "./components/TodoTemplate";
+import TodoInsert from "./components/TodoInsert";
+import { MdCrueltyFree } from "react-icons/md";
 
 const App = () => {
-  return <TodoTemplate>Todo App을 만들자!</TodoTemplate>;
+  return (
+    <TodoTemplate>
+      <TodoInsert />
+      <MdCrueltyFree />
+    </TodoTemplate>
+  );
 };
 
 export default App;
