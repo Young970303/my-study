0ada72dfbf2a9198829858ed864107a559367822
Young970303 <skwndbth88@naver.com>
2025-08-05 17:00:11 +0900

    일정 수정

--- todo-app/src/components/TodoListItem.js
+++ todo-app/src/components/TodoListItem.js
@@ -1,22 +1,19 @@
-import {
-  MdCheckBox,
-  MdCheckBoxOutlineBlank,
-  MdRemoveCircleOutline,
-} from 'react-icons/md';
+import { MdCheckBoxOutlineBlank, MdRemoveCircleOutline } from 'react-icons/md';
 import './TodoListItem.scss';
 
-const TodoListItem = () => {
+const TodoListItem = ({ todo }) => {
+  const { text, checked } = todo;
+
   return (
     <div className="TodoListItem">
-      <div className="checkbox">
+      <div className={`checkbox ${checked ? 'checked' : ''}`}>
         <MdCheckBoxOutlineBlank />
       </div>
-      <div className="text">할 일 </div>
+      <div className="text">{text}</div>
       <div className="remove">
         <MdRemoveCircleOutline />
       </div>
     </div>
   );
 };
-
 export default TodoListItem;
