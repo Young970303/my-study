ed7b711b2f883f6ca164b204b2a458841ec952e7
Young970303 <skwndbth88@naver.com>
2025-08-08 15:18:32 +0900

    todo 마무리

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,14 +1,49 @@
+import { useState, useRef, useCallback } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
+import './App.css';
 
-const App = () => {
+function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, text: '리액트 공부하기', checked: true },
+    { id: 2, text: '운동하기', checked: false },
+    { id: 3, text: '책 읽기', checked: false },
+  ]);
+
+  const nextId = useRef(4);
+
+  // 추가
+  const onInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    setTodos((todos) => todos.concat(todo));
+    nextId.current += 1;
+  }, []);
+
+  // 삭제
+  const onRemove = useCallback((id) => {
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  }, []);
+
+  // 토글
+  const onToggle = useCallback((id) => {
+    setTodos((todos) =>
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+      ),
+    );
+  }, []);
+
   return (
     <TodoTemplate>
-      <TodoInsert />
-      <TodoList />
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
   );
-};
+}
 
 export default App;
--- todo-app/src/components/TodoListItem.js
+++ todo-app/src/components/TodoListItem.js
@@ -1,22 +1,27 @@
 import {
-  MdCheckBoxOutlineBlank,
   MdCheckBox,
+  MdCheckBoxOutlineBlank,
   MdRemoveCircleOutline,
 } from 'react-icons/md';
 import './TodoListItem.scss';
 
-const TodoListItem = () => {
+function TodoListItem({ todo, onRemove, onToggle }) {
+  const { id, text, checked } = todo;
+
   return (
     <div className="TodoListItem">
-      <div className="checkbox">
-        <MdCheckBoxOutlineBlank />
+      <div
+        className={`checkbox ${checked ? 'checked' : ''}`}
+        onClick={() => onToggle(id)}
+      >
+        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+        <div className="text">{text}</div>
       </div>
-      <div className="text">할 일</div>
-      <div className="remove">
+      <div className="remove" onClick={() => onRemove(id)}>
         <MdRemoveCircleOutline />
       </div>
     </div>
   );
-};
+}
 
 export default TodoListItem;
